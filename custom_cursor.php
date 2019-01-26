0e0f20ea21953b59fb16376e3ad70a2590fdee88
Riccardo Di Curti <info@riccardodicurti.it>
2019-01-26 11:31:49 +0100

    [ 0.0.1 ] - WordPress Circular Cursor - roadmap

--- custom_cursor.php
+++ custom_cursor.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Plugin Name:       WordPress Plugin Boilerplate
+ * Plugin Name:       WordPress Circular Cursor
  * Plugin URI:        http://example.com/plugin-name-uri/
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
  * Version:           0.0.1
@@ -17,9 +17,15 @@ function rdc_wcc_enqueue_dependencies() {
 	wp_enqueue_style( 'rdc_wcc_style', plugin_dir_url( __FILE__ ) . 'public/css/custom_cursor_style.css');
   wp_enqueue_script( 'rdc_wcc_scripts', plugin_dir_url( __FILE__ ) . 'public/js/custom_cursor.js', array( 'jquery' ));
 }
-
 add_action( 'wp_enqueue_scripts', 'rdc_wcc_enqueue_dependencies');
 
-// settings
+
+/* TODO:
+ * v. 0.0.2 Inserire nel js tutte le definizioni base del css
+ * v. 0.0.3 Creare il menu lato backend per poter apportare tutte le modifiche del trader_cdl3starsinso
+ * v. 0.0.4 Salvare nel database tutte le impostazioni scelte dal backen
+ * v. 0.0.5 leggere il backend iun questo file e passare tutte le variabili al JS con la localizzazione
+ * v. 0.1.0 infiocchettare tutto e vedere come mettere il software nella repository di Wordpress
+ */
 
 ?>
