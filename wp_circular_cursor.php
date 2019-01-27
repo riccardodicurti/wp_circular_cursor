adc8bdb2b22a78a9e937467dca2c5009997db822
Riccardo Di Curti <info@riccardodicurti.it>
2019-01-27 12:33:48 +0100

    [ 0.1.0 ] - WordPress Circular Cursor - migliorato il menu di impostazioni

--- wp_circular_cursor.php
+++ wp_circular_cursor.php
@@ -3,7 +3,7 @@
  * Plugin Name:       WordPress Circular Cursor
  * Plugin URI:        http://example.com/plugin-name-uri/
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
- * Version:           0.0.1
+ * Version:           0.1.0
  * Author:            Riccardo Di Curti
  * Author URI:        http://example.com/
  * License:           GPL-2.0+
@@ -23,6 +23,7 @@ add_action( 'wp_enqueue_scripts', 'rdc_wcc_enqueue_dependencies');
 
 function rdc_wcc_enqueue_admin_dependencies() {
 	wp_enqueue_style( 'rdc_wcc_admin_style', plugin_dir_url( __FILE__ ) . 'admin/css/color_picker_style.min.css');
+	wp_enqueue_style( 'rdc_wcc_admin_style_2', plugin_dir_url( __FILE__ ) . 'admin/css/admin_page_style.min.css');
   wp_enqueue_script( 'rdc_wcc_admin_scripts', plugin_dir_url( __FILE__ ) . 'admin/js/wp-color-picker-alpha.js', array( 'wp-color-picker' ), false, true );
 }
 add_action( 'admin_enqueue_scripts', 'rdc_wcc_enqueue_admin_dependencies' );
@@ -30,10 +31,13 @@ add_action( 'admin_enqueue_scripts', 'rdc_wcc_enqueue_admin_dependencies' );
  function rdc_wcc_settings_init() {
   register_setting( 'rdc_wcc', 'rdc_wcc_options' );
   add_settings_section( 'rdc_wcc_section_developers', __( '', 'rdc_wcc' ), 'rdc_wcc_section_developers_cb', 'rdc_wcc' );
-	add_settings_field( 'rdc_wcc_field_cursore', __( 'Disattivare il cursore di base ?', 'rdc_wcc' ), 'rdc_wcc_field_cursore', 'rdc_wcc', 'rdc_wcc_section_developers', [ 'label_for' => 'rdc_wcc_field_cursore', 'class' => 'rdc_wcc_row', 'rdc_wcc_custom_data' => 'custom', ] );
-	add_settings_field( 'rdc_wcc_field_numero_di_palline', __( 'Numero di palline', 'rdc_wcc' ), 'rdc_wcc_field_numero_di_palline', 'rdc_wcc', 'rdc_wcc_section_developers', [ 'label_for' => 'rdc_wcc_field_numero_di_palline', 'class' => 'rdc_wcc_row', 'rdc_wcc_custom_data' => 'custom', ] );
-	add_settings_field( 'rdc_wcc_field_prima_pallina', __( 'Prima pallina', 'rdc_wcc' ), 'rdc_wcc_field_prima_pallina', 'rdc_wcc', 'rdc_wcc_section_developers', [ 'label_for' => 'rdc_wcc_field_prima_pallina', 'class' => 'rdc_wcc_row', 'rdc_wcc_custom_data' => 'custom', ] );
-	add_settings_field( 'rdc_wcc_field_seconda_pallina', __( 'Seconda pallina', 'rdc_wcc' ), 'rdc_wcc_field_seconda_pallina', 'rdc_wcc', 'rdc_wcc_section_developers', [ 'label_for' => 'rdc_wcc_field_seconda_pallina', 'class' => 'rdc_wcc_row', 'rdc_wcc_custom_data' => 'custom', ] );
+	add_settings_field( 'rdc_wcc_field_cursore', __( 'Turn off the basic cursor?', 'rdc_wcc' ), 'rdc_wcc_field_cursore', 'rdc_wcc', 'rdc_wcc_section_developers', [ 'label_for' => 'rdc_wcc_field_cursore', 'class' => 'rdc_wcc_row', 'rdc_wcc_custom_data' => 'custom', ] );
+	add_settings_field( 'rdc_wcc_field_numero_di_palline', __( 'Select the type of cursor', 'rdc_wcc' ), 'rdc_wcc_field_numero_di_palline', 'rdc_wcc', 'rdc_wcc_section_developers', [ 'label_for' => 'rdc_wcc_field_numero_di_palline', 'class' => 'rdc_wcc_row', 'rdc_wcc_custom_data' => 'custom', ] );
+	add_settings_field( 'rdc_wcc_field_colore_prima_pallina', __( 'Select the color of the first cursor', 'rdc_wcc' ), 'rdc_wcc_field_colore_prima_pallina', 'rdc_wcc', 'rdc_wcc_section_developers', [ 'label_for' => 'rdc_wcc_field_colore_prima_pallina', 'class' => 'rdc_wcc_row', 'rdc_wcc_custom_data' => 'custom', ] );
+	add_settings_field( 'rdc_wcc_field_dimensione_prima_pallina', __( 'Select the size of the first cursor', 'rdc_wcc' ), 'rdc_wcc_field_dimensione_prima_pallina', 'rdc_wcc', 'rdc_wcc_section_developers', [ 'label_for' => 'rdc_wcc_field_dimensione_prima_pallina', 'class' => 'rdc_wcc_row', 'rdc_wcc_custom_data' => 'custom', ] );
+	add_settings_field( 'rdc_wcc_field_colore_seconda_pallina', __( 'Select the color of the second cursor', 'rdc_wcc' ), 'rdc_wcc_field_colore_seconda_pallina', 'rdc_wcc', 'rdc_wcc_section_developers', [ 'label_for' => 'rdc_wcc_field_colore_seconda_pallina', 'class' => 'rdc_wcc_row', 'rdc_wcc_custom_data' => 'custom', ] );
+	add_settings_field( 'rdc_wcc_field_dimensione_seconda_pallina', __( 'Select the size of the second cursor', 'rdc_wcc' ), 'rdc_wcc_field_dimensione_seconda_pallina', 'rdc_wcc', 'rdc_wcc_section_developers', [ 'label_for' => 'rdc_wcc_field_dimensione_seconda_pallina', 'class' => 'rdc_wcc_row', 'rdc_wcc_custom_data' => 'custom', ] );
+	add_settings_field( 'rdc_wcc_field_ritardo_seconda_pallina', __( 'Select the delay of the second cursor respect to the first', 'rdc_wcc' ), 'rdc_wcc_field_ritardo_seconda_pallina', 'rdc_wcc', 'rdc_wcc_section_developers', [ 'label_for' => 'rdc_wcc_field_ritardo_seconda_pallina', 'class' => 'rdc_wcc_row', 'rdc_wcc_custom_data' => 'custom', ] );
  }
  add_action( 'admin_init', 'rdc_wcc_settings_init' );
 
@@ -55,30 +59,44 @@ add_action( 'admin_enqueue_scripts', 'rdc_wcc_enqueue_admin_dependencies' );
   ?>
   <select id="<?php echo esc_attr( $args['label_for'] ); ?>" data-custom="<?php echo esc_attr( $args['rdc_wcc_custom_data'] ); ?>" name="rdc_wcc_options[numero_di_palline]" >
 	  <option value="2" <?php echo isset( $rdc_wcc_options['numero_di_palline'] ) ? ( selected( $rdc_wcc_options['numero_di_palline'], '2', false ) ) : ( '' ); ?>>
-		  <?php esc_html_e( 'Due palline', 'rdc_wcc' ); ?>
+		  <?php esc_html_e( 'Double circular cursor', 'rdc_wcc' ); ?>
 	  </option>
 	  <option value="1" <?php echo isset( $rdc_wcc_options['numero_di_palline'] ) ? ( selected( $rdc_wcc_options['numero_di_palline'], '1', false ) ) : ( '' ); ?>>
-		  <?php esc_html_e( 'Una pallina', 'rdc_wcc' ); ?>
+		  <?php esc_html_e( 'Single circular cursor', 'rdc_wcc' ); ?>
 	  </option>
   </select>
 	<?php
  }
 
- function rdc_wcc_field_prima_pallina( $args ) {
+ function rdc_wcc_field_colore_prima_pallina( $args ) {
 	$rdc_wcc_options = get_option( 'rdc_wcc_options' );
 	echo '<input type="text" class="color-picker" data-alpha="true" data-default-color="rgba(0,0,0,0.85)" name="rdc_wcc_options[col_prima_pallina]" value="' . $rdc_wcc_options['col_prima_pallina'] . '"/>';
-	echo '<input type="number" class="dim_prima_pallina" name="rdc_wcc_options[dim_prima_pallina]" min="1" max="20" step="1" value="' . $rdc_wcc_options['dim_prima_pallina'] . '"/>';
  }
 
- function rdc_wcc_field_seconda_pallina( $args ) {
+ function rdc_wcc_field_dimensione_prima_pallina( $args ) {
+	 $rdc_wcc_options = get_option( 'rdc_wcc_options' );
+	 echo '<input type="number" class="dim_prima_pallina" name="rdc_wcc_options[dim_prima_pallina]" min="1" max="20" step="1" value="' . $rdc_wcc_options['dim_prima_pallina'] . '"/><span> px</span>';
+ }
+
+ function rdc_wcc_field_colore_seconda_pallina( $args ) {
 	$rdc_wcc_options = get_option( 'rdc_wcc_options' );
 	echo '<input type="text" class="color-picker" data-alpha="true" data-default-color="rgba(0,0,0,0.50)" name="rdc_wcc_options[col_seconda_pallina]" value="' . $rdc_wcc_options['col_seconda_pallina'] . '"/>';
-	echo '<input type="number" class="dim_seconda_pallina" name="rdc_wcc_options[dim_seconda_pallina]" min="1" max="20" step="1" value="' . $rdc_wcc_options['dim_seconda_pallina'] . '"/>';
-	echo '<input type="number" class="vel_seconda_pallina" name="rdc_wcc_options[vel_seconda_pallina]" min="50" max="800" step="50" value="' . $rdc_wcc_options['vel_seconda_pallina'] . '"/>';
+ }
+
+ function rdc_wcc_field_dimensione_seconda_pallina( $args ) {
+	$rdc_wcc_options = get_option( 'rdc_wcc_options' );
+	echo '<input type="number" class="dim_seconda_pallina" name="rdc_wcc_options[dim_seconda_pallina]" min="1" max="20" step="1" value="' . $rdc_wcc_options['dim_seconda_pallina'] . '"/><span> px</span>';
+ }
+
+ function rdc_wcc_field_ritardo_seconda_pallina( $args ) {
+ $rdc_wcc_options = get_option( 'rdc_wcc_options' );
+ echo '<input type="number" class="vel_seconda_pallina" name="rdc_wcc_options[vel_seconda_pallina]" min="50" max="800" step="50" value="' . $rdc_wcc_options['vel_seconda_pallina'] . '"/><span> millisecondi</span> ';
  }
 
  function rdc_wcc_options_page() {
-  add_menu_page( 'WordPress Circular Cursor', 'Circular Cursor', 'manage_options', 'rdc_wcc', 'rdc_wcc_options_page_html' );
+	// add_menu_page( string $page_title, string $menu_title, string $capability, string $menu_slug, callable $function = '', string $icon_url = '', int $position = null )
+  // add_menu_page( 'WordPress Circular Cursor', 'Circular Cursor', 'manage_options', 'rdc_wcc', 'rdc_wcc_options_page_html', 'dashicons-image-filter' );
+	add_options_page( 'WordPress Circular Cursor', 'Circular Cursor', 'manage_options', 'rdc_wcc', 'rdc_wcc_options_page_html' );
  }
  add_action( 'admin_menu', 'rdc_wcc_options_page' );
 
